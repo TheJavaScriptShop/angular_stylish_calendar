43d76eabbb33f311dd6c7e78768fe9ce3f8127f8
Sarvani Sanaboyina <sarvani@thejavascriptshop.com>
2023-02-07 15:27:18 +0530

    feat: add schematics- remove unused imports

--- projects/angular-calendar-ui/schematics/ng-add/index.ts
+++ projects/angular-calendar-ui/schematics/ng-add/index.ts
@@ -1,5 +1,4 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
 export function ngAdd(): Rule {
